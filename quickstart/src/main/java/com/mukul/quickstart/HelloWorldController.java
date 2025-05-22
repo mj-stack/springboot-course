3cedce7b84824658244f09ca583086dacefdb8c9
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-22 17:49:54 +0530

    Add HelloWorldController and VCS configuration files

---
+++ quickstart/src/main/java/com/mukul/quickstart/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.mukul.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "Hello mukul";
+    }
+}
+
